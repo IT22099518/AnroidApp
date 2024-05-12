2e95ba72eacd6af82e562a3a2ce52e173e4df199
IT22099518 <nuwanisithara.com@gmail.com>
2024-05-12 22:27:51 +0530

    Create add note activity and functions and buttons

---
+++ app/src/main/java/com/example/tasktrackerplus/AddNotesActivity.kt
@@ -0,0 +1,29 @@
+package com.example.tasktrackerplus
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tasktrackerplus.databinding.ActivityAddNotesBinding
+
+class AddNotesActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddNotesBinding
+    private lateinit var db: NotesDatabaseHelper
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAddNotesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = NotesDatabaseHelper(this)
+
+        binding.saveButton.setOnClickListener {
+            val title = binding.titleEditText.text.toString()
+            val content = binding.contentEditText.text.toString()
+            val note = Note(0, title, content)
+            db.insertNote(note)
+            finish()
+            Toast.makeText(this, "Note Saved", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
